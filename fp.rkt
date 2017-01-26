c50d9eb925f5555d7761020c719feefea7191776
Susan Bhattarai <susan.bhattarai@bison.howard.edu>
2017-01-26 18:31:41 -0500

    Added code for addressing when the list is nested

--- fp.rkt
+++ fp.rkt
@@ -6,4 +6,7 @@ Else: append the function call with cdr of list
 plus the head of list
 |#
 (define (reverse-general L)
+  (cond
+    ((null? L) #f) ;Return #f aka empty list
+    ((list? (car L)) (append (reverse-general (cdr l)) (list (reverse-general (car L))))) ;If head is list, append the function call on tail with function call on head
 )
